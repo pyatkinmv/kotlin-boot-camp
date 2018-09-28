1d2606251d366e447b9a3dafbfdf1a35d4933ff0
pyatkinmv <pyatkinmv@gmail.com>
2018-09-29 01:47:14 +0300

    Fixed gradle version error, fetched, assignment01 solved

--- assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
+++ assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
@@ -6,7 +6,11 @@ package io.rybalkinsd.kotlinbootcamp.assignments
  * @param values an argument. !! Assume values.length > 0. !!
  * @return the largest of values.
  */
-fun max(values: List<Int>): Int = TODO()
+fun max(values: List<Int>): Int {
+    var max: Int = values.first()
+    values.forEach { if (it > max) max = it }
+    return max
+}
 
 /**
  * Returns the sum of all `int` values.
@@ -14,4 +18,8 @@ fun max(values: List<Int>): Int = TODO()
  * @param values an argument. Assume values.length > 0.
  * @return the sum of all values.
  */
-fun sum(values: List<Int>): Long = TODO()
+fun sum(values: List<Int>): Long {
+    var sum: Long = 0
+    values.forEach {sum += it}
+    return sum
+}
